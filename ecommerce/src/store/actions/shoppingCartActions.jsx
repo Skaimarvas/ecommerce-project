48905fec919f0c1b1b88c3112f5c39b9fbfc5fc4
skaimarvas <skaimarvas@gmail.com>
2024-02-04 15:56:17 +0300

    postPaymentData actiion was created.

--- ecommerce/src/store/actions/shoppingCartActions.jsx
+++ ecommerce/src/store/actions/shoppingCartActions.jsx
@@ -2,6 +2,7 @@ export const CLEAR_CART = "CLEAR_CART";
 export const GET_ADDRESS = "GET_ADDRESS";
 export const POST_ORDERS = "POST_ORDERS";
 export const GET_PAYMENT = "GET_PAYMENT";
+export const POST_PAYMENT = "POST_PAYMENT";
 
 export const clearCart = () => {
   return {
@@ -23,6 +24,12 @@ export const postAddressData = (adress) => {
 export const getPaymentData = (payment) => {
   return {
     type: GET_PAYMENT,
+    payload: payment,
+  };
+};
+export const postPaymentData = (payment) => {
+  return {
+    type: POST_PAYMENT,
     payload: payment,
   };
 };
