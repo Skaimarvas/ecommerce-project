787b0e70a4749e3d3daa9e05b8b0062be0efffb4
skaimarvas <skaimarvas@gmail.com>
2024-02-04 15:44:37 +0300

    getProduct details were created

--- ecommerce/src/store/thunks/productThunk.jsx
+++ ecommerce/src/store/thunks/productThunk.jsx
@@ -1,4 +1,28 @@
-export const getProduct = (param) => {};
+import { toast } from "react-toastify";
+import { axiosInstance } from "../../api/api";
+import { FETCH_STATES } from "../actions/globalActions";
+import {
+  setProductFetchState,
+  getProductFromApi,
+  getTotalProductsCount,
+} from "../actions/productActions";
+
+export const getProduct = (param) => {
+  return (dispatch, getState) => {
+    dispatch(setProductFetchState(FETCH_STATES.fetching));
+    axiosInstance
+      .get(`products${param ? param : ""}`)
+      .then((res) => {
+        dispatch(getProductFromApi(res.data.products));
+        dispatch(getTotalProductsCount(res.data.total));
+        dispatch(setProductFetchState(FETCH_STATES.fetched));
+      })
+      .catch((err) => {
+        toast.error(err.message);
+        dispatch(setProductFetchState(FETCH_STATES.failed));
+      });
+  };
+};
 
 export const getBproduct = (param) => {};
 
