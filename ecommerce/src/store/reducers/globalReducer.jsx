ec23f13c295a1a0a3a41e82b18c4a824098d1168
skaimarvas <skaimarvas@gmail.com>
2024-02-04 13:52:13 +0300

    initialState was created on globalReducer

--- ecommerce/src/store/reducers/globalReducer.jsx
+++ ecommerce/src/store/reducers/globalReducer.jsx
@@ -1,3 +1,13 @@
+import { FETCH_STATES } from "../actions/globalActions";
+
+const initialState = {
+  roles: [],
+  categories: [],
+  theme: "",
+  language: "",
+  cfetchstate: FETCH_STATES.notFetched,
+};
+
 export const globalReducer = (state, action) => {
   switch (action.type) {
     default:
