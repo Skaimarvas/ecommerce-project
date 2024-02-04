9f31bebc30f03f3f543506495df1c14f46c20f6a
skaimarvas <skaimarvas@gmail.com>
2024-02-04 17:05:42 +0300

    getUserAddressData action was created

--- ecommerce/src/store/actions/userActions.jsx
+++ ecommerce/src/store/actions/userActions.jsx
@@ -1,7 +1,15 @@
 export const GET_USER_PAYMENT = "GET_USER_PAYMENT";
+export const GET_USER_ADDRESS = "GET_USER_ADDRESS";
+
 export const getUserPaymentData = (payment) => {
   return {
     type: GET_USER_PAYMENT,
     payload: payment,
+  };
+};
+export const getUserAddressData = (address) => {
+  return {
+    type: GET_USER_ADDRESS,
+    payload: address,
   };
 };
