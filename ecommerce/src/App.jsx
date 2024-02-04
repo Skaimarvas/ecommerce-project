64559db050d007c417727bb80274742573abc0e6
skaimarvas <skaimarvas@gmail.com>
2024-02-04 14:44:49 +0300

    empty spaces were deleted on App.jsx

--- ecommerce/src/App.jsx
+++ ecommerce/src/App.jsx
@@ -1,12 +1,10 @@
 import { useState } from "react";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
-
   return (
     <>
       <ToastContainer />
