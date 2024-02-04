c9042f6f8984a2230b07207675e8818997704e72
skaimarvas <skaimarvas@gmail.com>
2024-02-04 15:53:22 +0300

    getAddressData action was created

--- ecommerce/src/store/actions/shoppingCartActions.jsx
+++ ecommerce/src/store/actions/shoppingCartActions.jsx
@@ -1,7 +1,14 @@
 export const CLEAR_CART = "CLEAR_CART";
+export const GET_ADDRESS = "GET_ADDRESS";
 
 export const clearCart = () => {
   return {
     type: CLEAR_CART,
+  };
+};
+export const getAddressData = (adress) => {
+  return {
+    type: GET_ADDRESS,
+    payload: adress,
   };
 };
