6f515107ac8831aabcadce96525a1ff777ecd957
skaimarvas <skaimarvas@gmail.com>
2024-02-04 17:22:11 +0300

    POST_SIGNUP_DATA_TO_API case was created.

--- ecommerce/src/store/reducers/userReducer.jsx
+++ ecommerce/src/store/reducers/userReducer.jsx
@@ -1,4 +1,5 @@
 import { FETCH_STATES } from "../actions/globalActions";
+import { POST_SIGNUP_DATA_TO_API } from "../actions/userActions";
 
 const initialState = {
   newUsers: [],
@@ -10,6 +11,12 @@ const initialState = {
 
 export const userReducer = (state = initialState, action) => {
   switch (action.type) {
+    case POST_SIGNUP_DATA_TO_API:
+      const newUser = {
+        ...state,
+        newUsers: [...state.newUsers, { ...action.payload }],
+      };
+      return newUser;
     default:
       return state;
   }
