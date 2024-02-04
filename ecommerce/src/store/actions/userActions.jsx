37def0cec7c0f78a7fafed8580ebcc4e6ce3dbdb
skaimarvas <skaimarvas@gmail.com>
2024-02-04 17:17:16 +0300

    setUserFetchState action was created

--- ecommerce/src/store/actions/userActions.jsx
+++ ecommerce/src/store/actions/userActions.jsx
@@ -4,6 +4,7 @@ export const POST_SIGNUP_DATA_TO_API = "POST_SIGNUP_DATA_TO_API";
 export const POST_LOGIN_DATA_TO_API = "POST_LOGIN_DATA_TO_API";
 export const LOGOUT_USER = "LOGOUT_USER ";
 export const VERIFY_TOKEN = "VERIFY_TOKEN";
+export const SET_USER_FETCH_STATE = "SET_USER_FETCH_STATE";
 
 export const getUserPaymentData = (payment) => {
   return {
@@ -38,5 +39,11 @@ export const verifyUserToken = (tokenkey) => {
   return {
     type: VERIFY_TOKEN,
     payload: tokenkey,
+  };
+};
+export const setUserFetchState = (fetchstate) => {
+  return {
+    type: SET_USER_FETCH_STATE,
+    payload: fetchstate,
   };
 };
