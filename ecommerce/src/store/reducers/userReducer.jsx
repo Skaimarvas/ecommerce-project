03e627871217ea625226a3cedc0751ac886a2784
skaimarvas <skaimarvas@gmail.com>
2024-02-04 17:21:02 +0300

    initialState was created on userReducer

--- ecommerce/src/store/reducers/userReducer.jsx
+++ ecommerce/src/store/reducers/userReducer.jsx
@@ -1,4 +1,14 @@
-export const userReducer = (state, action) => {
+import { FETCH_STATES } from "../actions/globalActions";
+
+const initialState = {
+  newUsers: [],
+  user: {},
+  address: {},
+  payment: {},
+  userfetchstate: FETCH_STATES.notFetched,
+};
+
+export const userReducer = (state = initialState, action) => {
   switch (action.type) {
     default:
       return state;
