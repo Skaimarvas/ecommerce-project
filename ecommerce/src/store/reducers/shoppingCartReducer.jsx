4c3dd40ccb4647dde0fea09e2a3b0512c8dd37ab
skaimarvas <skaimarvas@gmail.com>
2024-02-04 16:19:10 +0300

    ADD_PRODUCT_TO_CART case was created.

--- ecommerce/src/store/reducers/shoppingCartReducer.jsx
+++ ecommerce/src/store/reducers/shoppingCartReducer.jsx
@@ -1,3 +1,5 @@
+import { ADD_PRODUCT_TO_CART } from "../actions/shoppingCartActions";
+
 const initialState = {
   cart: [],
   payments: [],
@@ -7,6 +9,23 @@ const initialState = {
 
 export const shoppingCartReducer = (state = initialState, action) => {
   switch (action.type) {
+    case ADD_PRODUCT_TO_CART:
+      const existingProduct = state.cart.findIndex(
+        (item) => item.product.id === action.payload.product.id
+      );
+
+      if (existingProduct !== -1) {
+        const updatedCart = [...state.cart];
+        updatedCart[existingProduct].count += action.payload.count;
+
+        return { ...state, cart: updatedCart };
+      } else {
+        return {
+          ...state,
+          cart: [...state.cart, action.payload],
+        };
+      }
+      break;
     default:
       return state;
   }
