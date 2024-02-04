4cdd5547f0d43d403e550a4d58b8b07f04a8d6e9
skaimarvas <skaimarvas@gmail.com>
2024-02-04 14:22:06 +0300

    axiosWithAuth was created and exported on api.jsx

--- ecommerce/src/api/api.jsx
+++ ecommerce/src/api/api.jsx
@@ -1,1 +1,17 @@
 import axios from "axios";
+
+export const axiosWithAuth = () => {
+  const token = JSON.parse(localStorage.getItem("token"));
+  return token
+    ? axios.create({
+        baseURL: "https://workintech-fe-ecommerce.onrender.com",
+        headers: {
+          Authorization: token,
+        },
+      })
+    : axios.create({
+        baseURL: "https://workintech-fe-ecommerce.onrender.com",
+      });
+};
+
+export let axiosInstance = axiosWithAuth();
