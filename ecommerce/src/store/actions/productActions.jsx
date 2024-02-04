706509c919b5fbebea4dff8fbdc14d994d8576c5
skaimarvas <skaimarvas@gmail.com>
2024-02-04 14:46:05 +0300

    getBestsFromApi action was created

--- ecommerce/src/store/actions/productActions.jsx
+++ ecommerce/src/store/actions/productActions.jsx
@@ -1,8 +1,15 @@
 export const GET_PRODUCT_FROM_API = "GET_PRODUCT_FROM_API";
+export const GET_BESTS_FROM_API = "GET_BESTS_FROM_API";
 
 export const getProductFromApi = (product) => {
   return {
     type: GET_PRODUCT_FROM_API,
     payload: product,
+  };
+};
+export const getBestsFromApi = (bests) => {
+  return {
+    type: GET_BESTS_FROM_API,
+    payload: bests,
   };
 };
