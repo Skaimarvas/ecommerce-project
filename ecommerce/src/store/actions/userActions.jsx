e14b9d00c11caba49634e49ede448b8956d9d9d3
skaimarvas <skaimarvas@gmail.com>
2024-02-04 17:08:38 +0300

    postLoginDataToApi action was created.

--- ecommerce/src/store/actions/userActions.jsx
+++ ecommerce/src/store/actions/userActions.jsx
@@ -1,6 +1,7 @@
 export const GET_USER_PAYMENT = "GET_USER_PAYMENT";
 export const GET_USER_ADDRESS = "GET_USER_ADDRESS";
 export const POST_SIGNUP_DATA_TO_API = "POST_SIGNUP_DATA_TO_API";
+export const POST_LOGIN_DATA_TO_API = "POST_LOGIN_DATA_TO_API";
 
 export const getUserPaymentData = (payment) => {
   return {
@@ -18,5 +19,11 @@ export const postSignupDataToApi = (sign) => {
   return {
     type: POST_SIGNUP_DATA_TO_API,
     payload: sign,
+  };
+};
+export const postLoginDataToApi = (login) => {
+  return {
+    type: POST_LOGIN_DATA_TO_API,
+    payload: login,
   };
 };
