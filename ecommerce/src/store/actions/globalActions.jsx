43008ca13e68ede20ceb592dcc772f4c7204bf53
skaimarvas <skaimarvas@gmail.com>
2024-02-04 13:43:40 +0300

    getRolesFromApi was created

--- ecommerce/src/store/actions/globalActions.jsx
+++ ecommerce/src/store/actions/globalActions.jsx
@@ -5,10 +5,16 @@ export const FETCH_STATES = {
   fetched: "FETCHED",
   failed: "FAILED",
 };
-
+export const GET_ROLES_FROM_API = "GET_ROLES_FROM_API";
 export const setCategoriesFetchState = (fetchstate) => {
   return {
     type: SET_CATEGORIES_FETCH_STATE,
     payload: fetchstate,
+  };
+};
+export const getRolesFromApi = (roles) => {
+  return {
+    type: GET_ROLES_FROM_API,
+    payload: roles,
   };
 };
