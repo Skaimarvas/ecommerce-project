e7cae7205ab01d1a85c2c414b2d7356e705fb075
skaimarvas <skaimarvas@gmail.com>
2024-02-04 14:47:27 +0300

    getTotalProductsCount was created

--- ecommerce/src/store/actions/productActions.jsx
+++ ecommerce/src/store/actions/productActions.jsx
@@ -1,5 +1,6 @@
 export const GET_PRODUCT_FROM_API = "GET_PRODUCT_FROM_API";
 export const GET_BESTS_FROM_API = "GET_BESTS_FROM_API";
+export const GET_TOTAL_PRODUCTS_COUNT = "GET_TOTAL_PRODUCTS_COUNT";
 
 export const getProductFromApi = (product) => {
   return {
@@ -11,5 +12,11 @@ export const getBestsFromApi = (bests) => {
   return {
     type: GET_BESTS_FROM_API,
     payload: bests,
+  };
+};
+export const getTotalProductsCount = (counts) => {
+  return {
+    type: GET_TOTAL_PRODUCTS_COUNT,
+    payload: counts,
   };
 };
