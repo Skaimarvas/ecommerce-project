db893b8718464eeb2ccd85ba9157bda7e4642898
skaimarvas <skaimarvas@gmail.com>
2024-02-04 15:02:53 +0300

    initialState was created

--- ecommerce/src/store/reducers/productReducer.jsx
+++ ecommerce/src/store/reducers/productReducer.jsx
@@ -1,4 +1,20 @@
-export const productReducer = (state, action) => {
+import { FETCH_STATES } from "../actions/globalActions";
+
+FETCH_STATES;
+
+const initalState = {
+  productlist: [],
+  bproductlist: [],
+  pdetail: "",
+  productcount: "",
+  pagecount: "",
+  activepage: "",
+  detailstate: FETCH_STATES.notFetched,
+  bfetchstate: FETCH_STATES.notFetched,
+  fetchstate: FETCH_STATES.notFetched,
+};
+
+export const productReducer = (state = initalState, action) => {
   switch (action.type) {
     default:
       return state;
