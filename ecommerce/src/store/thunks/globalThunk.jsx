bcd57e353166c92c94953100a19d78995ce7f85f
skaimarvas <skaimarvas@gmail.com>
2024-02-04 14:31:24 +0300

    getRoles created and exported on globalThunk

--- ecommerce/src/store/thunks/globalThunk.jsx
+++ ecommerce/src/store/thunks/globalThunk.jsx
@@ -1,3 +1,13 @@
-export const getRoles = () => {};
+import { getRolesFromApi } from "../actions/globalActions";
+import { axiosInstance } from "../../api/api";
+
+export const getRoles = () => {
+  return (dispath, getState) => {
+    axiosInstance
+      .get("/roles")
+      .then((res) => dispath(getRolesFromApi(res.data)))
+      .catch((err) => console.log(err.message));
+  };
+};
 
 export const getCategories = () => {};
