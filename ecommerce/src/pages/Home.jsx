d36931511bb1240570b6569d26622947aac60553
skaimarvas <skaimarvas@gmail.com>
2024-02-07 18:13:43 +0300

    EditorsPick was added on Home

--- ecommerce/src/pages/Home.jsx
+++ ecommerce/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 //Component
 import CarouselComp from "../components/CarouselComp";
+import EditorPick from "../components/EditorPick";
 
 export default function Home() {
   return (
     <>
       <CarouselComp />
+      <EditorPick />
     </>
   );
 }
