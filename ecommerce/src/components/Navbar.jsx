f213e5da891fc5f367f403fa2c63690671654ec9
skaimarvas <skaimarvas@gmail.com>
2024-02-07 16:04:49 +0300

    NavbarDark component was added on Navbar

--- ecommerce/src/components/Navbar.jsx
+++ ecommerce/src/components/Navbar.jsx
@@ -1,6 +1,12 @@
 import React from "react";
+//Components
+import NavbarDark from "./NavbarDark";
 
 export default function Navbar(props) {
   const { setConfirm } = props;
-  return <div className="flex flex-col gap-[12px] pb-[8px]">Navbar</div>;
+  return (
+    <div className="flex flex-col gap-[12px] pb-[8px]">
+      <NavbarDark />
+    </div>
+  );
 }
