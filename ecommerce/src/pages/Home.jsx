53fbc20d44a506d321c9be1693f1e00771903776
skaimarvas <skaimarvas@gmail.com>
2024-02-07 18:31:36 +0300

    CarouselMid component was added on Home

--- ecommerce/src/pages/Home.jsx
+++ ecommerce/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import CarouselComp from "../components/CarouselComp";
 import EditorPick from "../components/EditorPick";
 import BestsellerProduct from "../components/BestsellerProduct";
+import CarouselMid from "../components/CarouselMid";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <CarouselComp />
       <EditorPick />
       <BestsellerProduct />
+      <CarouselMid />
     </>
   );
 }
