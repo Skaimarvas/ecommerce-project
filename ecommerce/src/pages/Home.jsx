05b98075aecf68a57050f314334cecfb883f2a64
skaimarvas <skaimarvas@gmail.com>
2024-02-07 18:23:53 +0300

    BestsellerProduct was added on Homepage

--- ecommerce/src/pages/Home.jsx
+++ ecommerce/src/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from "react";
 //Component
 import CarouselComp from "../components/CarouselComp";
 import EditorPick from "../components/EditorPick";
+import BestsellerProduct from "../components/BestsellerProduct";
 
 export default function Home() {
   return (
     <>
       <CarouselComp />
       <EditorPick />
+      <BestsellerProduct />
     </>
   );
 }
