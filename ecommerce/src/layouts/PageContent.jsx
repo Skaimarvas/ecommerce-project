9f548909b83bba1b688352d0b169f1c92d941191
skaimarvas <skaimarvas@gmail.com>
2024-02-07 18:58:09 +0300

    ProductList page was added on PageContent

--- ecommerce/src/layouts/PageContent.jsx
+++ ecommerce/src/layouts/PageContent.jsx
@@ -3,11 +3,21 @@ import { Switch, Route, Redirect } from "react-router-dom";
 //Pages
 import Home from "../pages/Home";
 import NotFoundPage from "../pages/NotFoundPage";
+import ProductList from "../pages/ProductList";
 
 export default function PageContent() {
   return (
     <div>
       <Switch>
+        <Route path="/shopping/:cat/:id/:name" exact>
+          <Product />
+        </Route>
+        <Route path="/shopping" exact>
+          <ProductList />
+        </Route>
+        <Route path="/shopping/:gen/:cat" exact>
+          <ProductList />
+        </Route>
         <Route path="/" exact>
           <Home />
         </Route>
