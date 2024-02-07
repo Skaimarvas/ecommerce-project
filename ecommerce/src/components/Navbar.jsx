a43a403d0dd10623c2c38d64cc937e5b14ff8c01
skaimarvas <skaimarvas@gmail.com>
2024-02-07 15:52:30 +0300

    tailwind css design was added

--- ecommerce/src/components/Navbar.jsx
+++ ecommerce/src/components/Navbar.jsx
@@ -2,5 +2,5 @@ import React from "react";
 
 export default function Navbar(props) {
   const { setConfirm } = props;
-  return <div>Navbar</div>;
+  return <div className="flex flex-col gap-[12px] pb-[8px]">Navbar</div>;
 }
