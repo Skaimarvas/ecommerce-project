388da7623e9e32c22e777d09cad3c425e5750478
skaimarvas <skaimarvas@gmail.com>
2024-02-07 15:42:52 +0300

    Header component was added on App.jsx

--- ecommerce/src/App.jsx
+++ ecommerce/src/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from "react";
 import { ToastContainer } from "react-toastify";
+//Css
 import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
+//Components
+import Header from "./layouts/Header";
 
 function App() {
   const [count, setCount] = useState(0);
   return (
     <>
+      <Header />
       <ToastContainer />
     </>
   );
