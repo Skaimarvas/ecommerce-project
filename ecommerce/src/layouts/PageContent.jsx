5ec5e8a8b41836ba69be2321f6e4edc5126e7eff
skaimarvas <skaimarvas@gmail.com>
2024-02-07 17:19:57 +0300

    details were added

---
+++ ecommerce/src/layouts/PageContent.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
+//Pages
+
+export default function PageContent() {
+  return (
+    <div>
+      <Switch></Switch>
+    </div>
+  );
+}
