94ea25806c5f08f021e3f605ab6c9068e82f3a2e
skaimarvas <skaimarvas@gmail.com>
2024-02-07 17:09:17 +0300

    confirm component was  created

--- ecommerce/src/App.jsx
+++ ecommerce/src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from "react";
 import { ToastContainer } from "react-toastify";
+
 //Css
 import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
+
 //Components
 import Header from "./layouts/Header";
 import Footer from "./layouts/Footer";
 import ScrollToUpButton from "./components/ScrollToUpButton";
+
 function App() {
   const [confirm, setConfirm] = useState(false);
   return (
