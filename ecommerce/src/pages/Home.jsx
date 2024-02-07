ca1d673a9c32463ea4f0d35096a589a65f42941f
skaimarvas <skaimarvas@gmail.com>
2024-02-07 17:40:23 +0300

    CarouslComp was added on Home page

--- ecommerce/src/pages/Home.jsx
+++ ecommerce/src/pages/Home.jsx
@@ -1,5 +1,11 @@
 import React from "react";
+//Component
+import CarouselComp from "../components/CarouselComp";
 
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <CarouselComp />
+    </>
+  );
 }
