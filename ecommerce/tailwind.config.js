6184b09f6c2116de864255746173819b15b948cd
skaimarvas <skaimarvas@gmail.com>
2024-02-07 15:37:32 +0300

    tailwind material was installed

--- ecommerce/tailwind.config.js
+++ ecommerce/tailwind.config.js
@@ -6,3 +6,28 @@ export default {
   },
   plugins: [],
 };
+
+const withMT = require("@material-tailwind/react/utils/withMT");
+
+module.exports = withMT({
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      boxShadow: {
+        special: "10px 10px 5px 0px rgba(185,159,159,0.75)",
+      },
+    },
+    screens: {
+      "2xl": { max: "1535px" },
+
+      xl: { max: "1279px" },
+
+      lg: { max: "1023px" },
+
+      md: { max: "767px" },
+
+      sm: { max: "639px" },
+    },
+  },
+  plugins: [],
+});
