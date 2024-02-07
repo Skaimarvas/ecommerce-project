5d35cff2359582db454ba18ba3485872e4017ddf
skaimarvas <skaimarvas@gmail.com>
2024-02-07 16:18:03 +0300

    NavbarLight component was added on Navbar

--- ecommerce/src/components/Navbar.jsx
+++ ecommerce/src/components/Navbar.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 //Components
 import NavbarDark from "./NavbarDark";
+import NavbarLight from "./NavbarLight";
 
 export default function Navbar(props) {
   const { setConfirm } = props;
   return (
     <div className="flex flex-col gap-[12px] pb-[8px]">
       <NavbarDark />
+      <NavbarLight setConfirm={setConfirm} />
     </div>
   );
 }
